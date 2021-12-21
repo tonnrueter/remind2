58a90fda317bbcabfe9439508dae3d89d0c20592
Christof Schoetz <math@christof-schoetz.de>
2021-12-21 11:59:02 +0100

    CompareScenarios2: * handle YAML params non-explicitly * Rmd-output * figureSize as YAML param * cs_plot_functions as sourced R file

--- R/compareScenarios2.R
+++ R/compareScenarios2.R
@@ -3,45 +3,56 @@
 #' Renders the CompareScenarios2.Rmd. In the Rmd, Scenario- and historical .mif-files are loaded. Plots are created from
 #' this data. The result may be rendered to PDF or html.
 #'
-#' @param mif character(n). Path to one or more mif-files.
-#' @param hist character(1). Path to one mif-file containing historical data.
-#' @param y numerical(n). Years for which scenario data will be shown.
-#' @param y_hist numerical(n). Years for which historical data will be shown.
-#' @param y_bar numerical(n). Years to be shown in bar plots.
-#' @param reg character(n) or NULL. Regions to be shown. NULL for all.
+#' @param ... YAML parameters.
 #' @param outputFile character(1). File name of the output document to be created (without extension).
 #' @param outputDir character(1). The directory where the output document and intermediary files are created.
-#' @param outputFormat character(1). "html_document" or "pdf_document".
-#' @param ... Further arguments to be passed to rmarkdown::render().
-#' @value The value returned by rmarkdown::render() is returned.
+#' @param outputFormat character(1). "html_document", "pdf_document", or "rmd".
+#' @return The value returned by rmarkdown::render() is returned.
 #' @author Christof Schoetz
 #' @export
-compareScenarios2 <- function(mif,
-  hist,
-  y = c(seq(2005, 2060, 5), seq(2070, 2100, 10)),
-  y_hist = c(seq(1960, 2020, 1), seq(2025, 2100, 5)),
-  y_bar = c(2010, 2030, 2050, 2100),
-  reg = NULL,
-  mainReg = "World",
+compareScenarios2 <- function(
+  ...,
   outputDir = getwd(),
   outputFile = "CompareScenarios2",
-  outputFormat = "html_document",
-  ...) {
-  yaml_params <- list(
-    mifScen = mif,
-    mifHist = hist,
-    yearsScen = y,
-    yearshist = y_hist,
-    yearsBarPlot = y_bar,
-    reg = reg,
-    mainReg = mainReg)
+  outputFormat = "html_document"
+  ) {
+  yamlParams <- list(...)
+  if (outputFormat == "rmd") {
+    return(.compareScenarios2_rmd(yamlParams, outputDir, outputFile))
+  }
   rmarkdown::render(
     system.file("markdown/compareScenarios2/cs2_main.Rmd", package = "remind2"),
     intermediates_dir = outputDir,
     output_dir = outputDir,
     output_file = outputFile,
     output_format = outputFormat,
-    params = yaml_params,
-    envir = new.env(),
-    ...)
+    params = yamlParams,
+    envir = new.env())
 }
+
+.compareScenarios2_rmd <- function(yamlParams, outputDir, outputFile) {
+  system.file("markdown/compareScenarios2/cs2_main.Rmd", package = "remind2") %>% 
+    ymlthis:::read_rmd() %>% 
+    yaml::yaml.load(handlers=list(r = \(x) ymlthis::yml_params_code(!!rlang::parse_expr(x)))) %>% 
+    ymlthis::as_yml() ->
+    baseYaml
+  baseYaml %>% 
+    ymlthis::yml_pluck("params") ->
+    newYamlParams
+  newYamlParams[names(yamlParams)] <- yamlParams
+  baseYaml %>% 
+    ymlthis::yml_replace(params = newYamlParams) ->
+    yaml
+  pathDir <- file.path(outputDir, paste0(outputFile, "_Rmd"))
+  if (!dir.exists(pathDir)) dir.create(pathDir)
+  system.file("markdown/compareScenarios2", package = "remind2") %>% 
+    dir(full.names = TRUE) %>% 
+    grep(pattern="cs2_main\\.Rmd$", invert=TRUE, value=TRUE) %>% 
+    file.copy(pathDir)
+  ymlthis::use_rmarkdown(
+    yaml,
+    path = file.path(pathDir, "cs2_main.Rmd"),
+    template = system.file("markdown/compareScenarios2/cs2_main.Rmd", package = "remind2"),
+    include_yaml = FALSE)
+}
+
---
+++ inst/markdown/compareScenarios2/cs2_plot_functions.R
@@ -0,0 +1,222 @@
+showAreaAndBarPlots <- function(data, items, tot = NULL) {
+  # This function uses the 'global' variables: mainReg, yearsBarPlot.
+  
+  data %>% 
+    filter(variable %in% items, scenario != "historical") ->
+    d
+  if (NROW(d) == 0) return(invisible(NULL))
+  
+  # order variables by mean value
+  d %>% 
+    mutate(variable = fct_reorder(variable, value, mean, na.rm=TRUE)) ->
+    d
+  
+  # Create plots.
+  
+  d %>% 
+    filter(region == mainReg) %>% 
+    mipArea(scales="free_y", total = is.null(tot)) + 
+    theme(legend.position="none") ->
+    p1
+  if (!is.null(tot)) {
+    p1 <- p1 +
+      geom_line(
+        data = data %>% 
+          filter(region == mainReg, variable == tot, scenario != "historical"), 
+        mapping = aes(period, value),
+        size=1.3)
+  }
+  
+  d %>% 
+    filter(region == mainReg, period %in% yearsBarPlot) %>% 
+    mipBarYearData() +
+    theme(legend.position="none") ->
+    p2
+  
+  d %>% 
+    filter(region != mainReg, period %in% yearsBarPlot) %>% 
+    mipBarYearData() +
+    guides(fill=guide_legend(reverse=TRUE, ncol=3)) ->
+    p3
+  
+  d %>% 
+    filter(region != mainReg) %>% 
+    mipArea(scales="free_y", total = is.null(tot)) +
+    guides(fill=guide_legend(reverse=TRUE)) ->
+    p4
+  
+  # Show plots.
+  grid.arrange(p1, p2, p3, layout_matrix = rbind(c(1, 3), c(2, 3)))
+  plot(p4)
+  
+  return(invisible(NULL))
+}
+
+
+showLinePlots <- function(data, filter_variable=NULL) {
+  # This function uses the 'global' variables: mainReg
+  
+  if (!is.null(filter_variable)) {
+    d <- filter(data, variable %in% filter_variable)
+  } else {
+    d <- data
+  }
+  d %>% 
+    filter(region == mainReg, scenario != "historical") ->
+    dMainScen
+  d %>% 
+    filter(region == mainReg, scenario == "historical") ->
+    dMainHist
+  d %>% 
+    filter(region != mainReg, scenario != "historical") ->
+    dRegiScen
+  d %>% 
+    filter(region != mainReg, scenario == "historical") ->
+    dRegiHist
+  label <- paste0(filter_variable, " (", paste0(unique(d$unit), collapse="|"), ")")
+  
+  if (NROW(dMainScen) == 0 && NROW(dRegiScen) == 0) return(invisible(NULL))
+  if (NROW(dMainScen) == 0) {
+    p1 <- grid::grid.rect(gp=grid::gpar(col=NA))
+  } else {
+   dMainScen %>% 
+      mipLineHistorical(
+        x_hist = d %>% filter(region == mainReg, scenario == "historical"),
+        ylab = label,
+        scales = "free_y",
+        plot.priority = c("x_hist","x","x_proj")) ->
+      p1
+  }
+  if (NROW(dRegiScen) == 0) {
+    p2 <- grid::grid.rect(gp=grid::gpar(col=NA))
+  } else {
+    dRegiScen %>% 
+      mipLineHistorical(
+        x_hist = d %>% filter(region != mainReg, scenario == "historical"),
+        ylab = NULL,
+        scales = "free_y",
+        plot.priority = c("x_hist","x","x_proj"),
+        facet.ncol = 3) ->
+      p2
+  }
+  
+  grid.arrange(p1, p2, nrow=1)
+  
+  return(invisible(NULL))
+}
+
+
+showLinePlotsWithTarget <- function(data, filter_variable) {
+  filter_variable %>% 
+    paste0("|target|") %>% 
+    str_replace_all(fixed("|"), fixed("\\|")) %>% 
+    paste0(collapse="|") ->
+    target_pattern
+  
+  data %>% 
+    filter(str_detect(variable, target_pattern)) ->
+    dTar
+  regionsWithTarget <- unique(dTar$region)
+  data %>% 
+    filter(variable %in% filter_variable, region %in% regionsWithTarget) ->
+    d
+  if (NROW(d) == 0) return(invisible(NULL))
+  
+  label <- paste0(filter_variable, " (", paste0(unique(d$unit), collapse="|"), ")")
+  d %>% 
+    filter(scenario != "historical") %>% 
+    mipLineHistorical(
+      x_hist = d %>% filter(scenario == "historical"),
+      ylab = label,
+      scales = "free_y",
+      plot.priority = c("x_hist","x","x_proj"),
+      facet.ncol = 3) + 
+    geom_hline(data = dTar, aes(yintercept=value), linetype=2, color = "coral") +
+    geom_vline(data = dTar, aes(xintercept=period), linetype=2, color = "coral") +
+    geom_text(data = dTar, aes(x=max(d$period) - (max(d$period) - min(d$period)) / 4, y=value, label = paste(variable, period))) ->
+    p
+  print(p)
+  
+  return(invisible(NULL))
+}
+
+
+showMultiLinePlots <- function(data, items) {
+  # This function uses the 'global' variables: mainReg.
+  
+  data %>% 
+    filter(variable %in% items) ->
+    d
+  
+  label <- paste0("(", paste0(unique(d$unit), collapse="|"), ")")
+  
+  d %>% 
+    filter(region == mainReg, scenario != "historical") %>% 
+    ggplot(aes(period, value)) + 
+    facet_wrap(vars(variable), scales="free_y") +
+    geom_line(aes(linetype=scenario)) +
+    geom_point(
+      data = d %>% filter(region == mainReg, scenario == "historical"), 
+      aes(shape = model)) +
+    theme_minimal() +
+    ylab(label) ->
+    p1
+  
+  d %>% 
+    filter(region != mainReg, scenario != "historical") %>% 
+    ggplot(aes(period, value, color=region)) + 
+    facet_wrap(vars(variable), scales="free_y") +
+    geom_line(aes(linetype=scenario)) +
+    geom_point(
+      data = d %>% filter(region != mainReg, scenario == "historical"), 
+      aes(shape = model)) +
+    theme_minimal() +
+    ylab(label) ->
+    p2
+  
+  print(p1)
+  print(p2)
+  
+  return(invisible(NULL))
+}
+
+
+showMultiLinePlotsByGDP <- function(data, items) {
+  # This function uses the 'global' variables: mainReg.
+  
+  data %>% 
+    filter(variable %in% items) ->
+    d
+  
+  label <- paste0("(", paste0(unique(d$unit), collapse="|"), ")")
+  
+  d %>% 
+    filter(region == mainReg, scenario != "historical") %>% 
+    ggplot(aes(gdp, value)) + 
+    facet_wrap(vars(variable), scales="free_y") +
+    geom_line(aes(linetype=scenario)) +
+    geom_point(
+      data = d %>% filter(region == mainReg, scenario == "historical"), 
+      aes(shape = model)) +
+    theme_minimal() +
+    ylab(label) + xlab("GDP PPP pCap (kUS$2005)") ->
+    p1
+  
+  d %>% 
+    filter(region != mainReg, scenario != "historical") %>% 
+    ggplot(aes(gdp, value, color=region)) + 
+    facet_wrap(vars(variable), scales="free_y") +
+    geom_line(aes(linetype=scenario)) +
+    geom_point(
+      data = d %>% filter(region != mainReg, scenario == "historical"), 
+      aes(shape = model)) +
+    theme_minimal() +
+    ylab(label) + xlab("GDP PPP pCap (kUS$2005)") ->
+    p2
+  
+  print(p1)
+  print(p2)
+  
+  return(invisible(NULL))
+}
+
