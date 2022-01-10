3ce9c9df355ccb22bb1cd61060899dfd8632c430
Christof Schoetz <math@christof-schoetz.de>
2022-01-10 11:32:42 +0100

    discard old compareScenarios changes

--- R/compareScenarios.R
+++ R/compareScenarios.R
@@ -317,8 +317,10 @@ compareScenarios <- function(mif, hist,
   swlatex(sw,"\\tableofcontents\\newpage")
   
   ## empty page
-  swlatex(sw,"\\onecolumn\\newpage\\twocolumn")
-  
+  swlatex(sw,"\\newpage")
+  swlatex(sw,"\\thispagestyle{empty}")
+  swlatex(sw,"\\mbox{}")
+  swlatex(sw,"\\newpage")
   
   ## ---- ++++ S U M M A R Y ++++ ----
   
@@ -329,14 +331,14 @@ compareScenarios <- function(mif, hist,
   swlatex(sw,"\\subsection{GHG Emissions}")
   
   tot <-"Emi|GHG (Mt CO2eq/yr)"
-  items <- items <- c(
-    rev(c("Emi|CO2|Energy (Mt CO2/yr)",
-          "Emi|CO2|Industrial Processes (Mt CO2/yr)",
-          "Emi|CO2|Land-Use Change (Mt CO2/yr)",
-          "Emi|GHG|CH4 (Mt CO2eq/yr)",
-          "Emi|GHG|N2O (Mt CO2eq/yr)",
-          "Emi|GHG|F-Gases (Mt CO2eq/yr)")),
-    "Emi|CO2|non-BECCS CDR (Mt CO2/yr)")
+  items <- c("Emi|CO2|Energy (Mt CO2/yr)",
+             "Emi|CO2|Industrial Processes (Mt CO2/yr)",
+             "Emi|CO2|Land-Use Change (Mt CO2/yr)",
+             "Emi|GHG|CH4 (Mt CO2eq/yr)",
+             "Emi|GHG|N2O (Mt CO2eq/yr)",
+             "Emi|GHG|F-Gases (Mt CO2eq/yr)",
+             "Emi|CO2|non-BECCS CDR (Mt CO2/yr)")
+  
   
   var <- data[,,intersect(items,getNames(data,dim=3))]
   p <- mipArea(var[mainReg,,],scales="free_y", total = F)
@@ -349,7 +351,7 @@ compareScenarios <- function(mif, hist,
 
   p <- mipBarYearData(var[mainReg,y_bar,])
   p <- p + theme(legend.position="none")
-  swfigure(sw,print,p,sw_option="height=4.0,width=7")
+  swfigure(sw,print,p,sw_option="height=4.5,width=7")
 
   p <- mipBarYearData(var[,y_bar,][mainReg,,,invert=TRUE]) +
         guides(fill=guide_legend(ncol=3))
@@ -360,7 +362,7 @@ compareScenarios <- function(mif, hist,
   swfigure(sw,print,p,sw_option="height=8,width=16")
   swlatex(sw,"\\twocolumn")
   
-
+  
   ## ---- GHG by sector ----
   
   swlatex(sw,"\\subsection{GHG by sector (w/ gross emissions, excl. BECCS)}")
@@ -920,38 +922,26 @@ compareScenarios <- function(mif, hist,
     theme(legend.position="right")
   swfigure(sw,print,p,sw_option="height=9,width=16")
   
-  d <- data[mainReg,,"Price|Carbon|ETS (US$2005/t CO2)"]
-  if (isTRUE(any(d!=0))) {
-    swlatex(sw,"\\twocolumn")
-    p <- mipLineHistorical(d,x_hist=NULL,
-                           ylab='Price|Carbon|ETS [US$2005/t CO2]',scales="free_y",plot.priority=c("x_hist","x","x_proj"))
-    p <- p + theme(legend.position="none")
-    swfigure(sw,print,p,sw_option="height=8,width=8")
-  }
-  d <- data[,,"Price|Carbon|ETS (US$2005/t CO2)"][mainReg,,,invert=TRUE]
-  if (isTRUE(any(d!=0))) {
-    p <- mipLineHistorical(d,x_hist=NULL,
-                           ylab='Price|Carbon|ETS [US$2005/t CO2]',scales="free_y",plot.priority=c("x_hist","x","x_proj"),facet.ncol=3)
-    swfigure(sw,print,p,sw_option="height=9,width=8")
-  }
-  
-  d <- data[mainReg,,"Price|Carbon|ESR (US$2005/t CO2)"]
-  if (isTRUE(any(d!=0))) {
-    swlatex(sw,"\\twocolumn")
-    p <- mipLineHistorical(,x_hist=NULL,
-                           ylab='Price|Carbon|ESR [US$2005/t CO2]',scales="free_y",plot.priority=c("x_hist","x","x_proj"))
-    p <- p + theme(legend.position="none")
-    swfigure(sw,print,p,sw_option="height=8,width=8")
-  }
-  d <- data[,,"Price|Carbon|ESR (US$2005/t CO2)"][mainReg,,,invert=TRUE]
-  if (isTRUE(any(d!=0))) {
-    p <- mipLineHistorical(d,x_hist=NULL,
-                           ylab='Price|Carbon|ESR [US$2005/t CO2]',scales="free_y",plot.priority=c("x_hist","x","x_proj"),facet.ncol=3)
-    swfigure(sw,print,p,sw_option="height=9,width=8")
-  }
   swlatex(sw,"\\twocolumn")
+  p <- mipLineHistorical(data[mainReg,,"Price|Carbon|ETS (US$2005/t CO2)"],x_hist=NULL,
+                         ylab='Price|Carbon|ETS [US$2005/t CO2]',scales="free_y",plot.priority=c("x_hist","x","x_proj"))
+  p <- p + theme(legend.position="none")
+  swfigure(sw,print,p,sw_option="height=8,width=8")
+  p <- mipLineHistorical(data[,,"Price|Carbon|ETS (US$2005/t CO2)"][mainReg,,,invert=TRUE],x_hist=NULL,
+                         ylab='Price|Carbon|ETS [US$2005/t CO2]',scales="free_y",plot.priority=c("x_hist","x","x_proj"),facet.ncol=3)
+  swfigure(sw,print,p,sw_option="height=9,width=8")
   
+  swlatex(sw,"\\twocolumn")
+  p <- mipLineHistorical(data[mainReg,,"Price|Carbon|ESR (US$2005/t CO2)"],x_hist=NULL,
+                         ylab='Price|Carbon|ESR [US$2005/t CO2]',scales="free_y",plot.priority=c("x_hist","x","x_proj"))
+  p <- p + theme(legend.position="none")
+  swfigure(sw,print,p,sw_option="height=8,width=8")
+  p <- mipLineHistorical(data[,,"Price|Carbon|ESR (US$2005/t CO2)"][mainReg,,,invert=TRUE],x_hist=NULL,
+                         ylab='Price|Carbon|ESR [US$2005/t CO2]',scales="free_y",plot.priority=c("x_hist","x","x_proj"),facet.ncol=3)
+  swfigure(sw,print,p,sw_option="height=9,width=8")
   
+  swlatex(sw,"\\twocolumn")
+
   if("Policy Cost|Consumption Loss (billion US$2005/yr)" %in% magclass::getNames(data,dim=3)) {
     ## ---- Policy Cost|Consumption Loss ----
     swlatex(sw,"\\subsection{Policy Costs}")
@@ -1580,49 +1570,27 @@ compareScenarios <- function(mif, hist,
   ## ---- Kaya decomposition ----
 
   swlatex(sw,"\\subsection{Kaya-Decomposition}")
-  
   ## calculate Kaya-Decomposition
   kaya <- new.magpie(getRegions(data),getYears(data),magclass::getNames(data,dim=1))
-  names(dimnames(kaya))[3] <- "scenario"
-  kaya <- add_dimension(kaya,dim=3.2,add="model",nm=magclass::getNames(data,dim=2))
-  kaya_vars <- c("CO2 FF&I/FE","FE/GDP","GDP/Population","Population")
-  kaya <- add_dimension(kaya,dim=3.3,add="variable",nm=kaya_vars)
-  
+  kaya <- add_dimension(kaya,dim=3.2,add="kaya",nm=c("CO2 FF&I/FE","FE/GDP","GDP/Population","Population"))
   for (i in magclass::getNames(data,dim=1)) {
     kaya[,,i] <- calcKayaDecomp(mif=data[,,i])
   }
-  p <- magpie2ggplot2(kaya,facet_y="Data3",facet_x="Data1",color="Region",group=NULL,
+  p <- magpie2ggplot2(kaya,facet_y="Data2",facet_x="Data1",color="Region",group=NULL,
                       scales="free_y",show_grid=TRUE,ylab='Kaya Decomposition [%]',
                       color_pal=plotstyle(getRegions(kaya)))
   swfigure(sw,print,p,sw_option="height=10,width=9")
-  
-  
-  kaya2 <- new.magpie(getRegions(data),getYears(data),magclass::getNames(data,dim=1))
-  kaya2 <- add_dimension(kaya2,dim=3.2,add="kaya",nm=c("CO2 FF&I [Mt CO2/yr]/FE [EJ/yr]","FE [EJ/yr]/GDP [billion US$2005/yr]",
+
+  kaya <- new.magpie(getRegions(data),getYears(data),magclass::getNames(data,dim=1))
+  kaya <- add_dimension(kaya,dim=3.2,add="kaya",nm=c("CO2 FF&I [Mt CO2/yr]/FE [EJ/yr]","FE [EJ/yr]/GDP [billion US$2005/yr]",
                                                      "GDP [billion US$2005/yr]/Population [million]","Population [million]"))
   for (i in magclass::getNames(data,dim=1)) {
-    kaya2[,,i] <- calcKayaDecomp(mif=data[,,i],ref_year=NULL)
+    kaya[,,i] <- calcKayaDecomp(mif=data[,,i],ref_year=NULL)
   }
-  p <- magpie2ggplot2(kaya2,facet_y="Data2",facet_x="Data1",color="Region",group=NULL,
+  p <- magpie2ggplot2(kaya,facet_y="Data2",facet_x="Data1",color="Region",group=NULL,
                       scales="free_y",show_grid=TRUE,ylab='Kaya Decomposition',
-                      color_pal=plotstyle(getRegions(kaya2)))
+                      color_pal=plotstyle(getRegions(kaya)))
   swfigure(sw,print,p,sw_option="height=10,width=9")
-  
-  
-  for (kv in kaya_vars) {
-    swlatex(sw,paste0("\\subsubsection{Kaya-Decomposition -- ",kv,"}"))
-    p <- mipLineHistorical(kaya[mainReg,,kv],
-                           ylab=paste0(kv, ' - Kaya Decomposition [%]'),
-                           scales="free_y")
-    swfigure(sw,print,p,sw_option="height=8,width=8")
-    p <- mipLineHistorical(kaya[,,kv][mainReg,,,invert=TRUE],
-                           ylab=paste0(kv, ' - Kaya Decomposition [%]'),scales="free_y",
-                           plot.priority=c("x_hist","x","x_proj"),facet.ncol=3)
-    swfigure(sw,print,p,sw_option="height=9,width=8")
-  }
-  
-
-  
 
   ## ---- ++++ E M I S S I O N S ++++ ----
 
@@ -1660,6 +1628,109 @@ compareScenarios <- function(mif, hist,
   p <- mipLineHistorical(data[,,"Emi|GHG|w/ Bunkers (Mt CO2eq/yr)"][mainReg,,,invert=TRUE],x_hist=NULL,
                          ylab='Total GHG Emisions w/ Bunkers [Mt CO2-equiv/yr]',scales="free_y",plot.priority=c("x_hist","x","x_proj"),facet.ncol=3)
   swfigure(sw,print,p,sw_option="height=9,width=8")
+  
+  ## ---- Total Emissions with LULLUCF national accounting correction ----
+  
+  if ("Emi|GHG|LULUCF national accounting (Mt CO2eq/yr)" %in% getNames(data)) {
+  
+  ## ---- GHG by sector ----
+  
+  swlatex(sw,"\\subsection{GHG by sector (w/ gross emissions and national LULUCF accounting correction)}")
+  
+  tot <- "Emi|GHG|LULUCF national accounting (Mt CO2eq/yr)"
+  items <- c("Emi|GHG|Gross|Energy|Supply|Electricity (Mt CO2eq/yr)",
+             "Emi|GHG|Gross|Energy|Supply|Non-electric (Mt CO2eq/yr)",
+             "Emi|GHG|Energy|Demand|Transport (Mt CO2eq/yr)",
+             "Emi|GHG|Energy|Demand|Buildings (Mt CO2eq/yr)",
+             "Emi|GHG|Gross|Energy|Demand|Industry (Mt CO2eq/yr)",
+             "Emi|GHG|Industrial Processes (Mt CO2eq/yr)",
+             "Emi|GHG|Agriculture (Mt CO2eq/yr)",
+             "Emi|GHG|Land-Use Change|LULUCF national accounting (Mt CO2eq/yr)",
+             "Emi|GHG|Waste (Mt CO2eq/yr)",
+             "Emi|GHG|F-Gas (Mt CO2eq/yr)",
+             "Emi|CO2|CDR|BECCS (Mt CO2/yr)",
+             "Emi|CO2|CDR|Industry CCS|Synthetic Fuels (Mt CO2/yr)",
+             "Emi|CO2|CDR|DACCS (Mt CO2/yr)",
+             "Emi|CO2|CDR|EW (Mt CO2/yr)")
+  
+  
+  var <- data[,,intersect(items,getNames(data,dim=3))]
+  p <- mipArea(var[mainReg,,],scales="free_y", total = F)
+  p <- p + theme(legend.position="none") + 
+    scale_y_continuous("Emi|GHG (Mt CO2eq/yr)") +
+    geom_line(data=as.quitte(data[mainReg,,tot]), 
+              mapping=aes(period, value),
+              size=1.3)
+  swfigure(sw,print,p,sw_option="height=3.5,width=7")
+  
+  p <- mipBarYearData(var[mainReg,y_bar,])
+  p <- p + theme(legend.position="none")
+  swfigure(sw,print,p,sw_option="height=4.5,width=7")
+  
+  p <- mipBarYearData(var[,y_bar,][mainReg,,,invert=TRUE]) +
+    guides(fill=guide_legend(ncol=3))
+  swfigure(sw,print,p,sw_option="height=9,width=8")
+  
+  swlatex(sw,"\\onecolumn")
+  p <- mipArea(var[mainReg,,,invert=TRUE],scales="free_y", total = F)
+  swfigure(sw,print,p,sw_option="height=8,width=16")
+  swlatex(sw,"\\twocolumn")
+  
+  ## ---- CO2 by sector ----
+  
+  
+ 
+  
+  swlatex(sw,"\\subsection{CO2 by sector (w/ gross emissions and national LULUCF accounting correction)}")
+  
+  tot <-"Emi|CO2|LULUCF national accounting (Mt CO2/yr)"
+  items <- c(
+    "Emi|CO2|Land-Use Change|LULUCF national accounting (Mt CO2/yr)",
+    "Emi|CO2|Industrial Processes (Mt CO2/yr)",
+    "Emi|CO2|Energy|Demand|Transport (Mt CO2/yr)",
+    "Emi|CO2|Gross|Energy|Demand|Industry (Mt CO2/yr)",
+    "Emi|CO2|Energy|Demand|Buildings (Mt CO2/yr)",
+    "Emi|CO2|Gross|Energy|Supply|Non-electric (Mt CO2/yr)",
+    "Emi|CO2|Gross|Energy|Supply|Electricity (Mt CO2/yr)",
+    "Emi|CO2|CDR|BECCS (Mt CO2/yr)",
+    "Emi|CO2|CDR|Industry CCS|Synthetic Fuels (Mt CO2/yr)",
+    "Emi|CO2|CDR|DACCS (Mt CO2/yr)",
+    "Emi|CO2|CDR|EW (Mt CO2/yr)")
+  
+  
+  var <- data[,,intersect(items,getNames(data,dim=3))]
+  
+  p <- mipArea(var[mainReg,,],scales="free_y", total = F)
+  p <- p + theme(legend.position="bottom") + 
+    scale_y_continuous("Emi|CO2 (Mt CO2/yr)") +
+    geom_line(data=as.quitte(data[mainReg,,tot]), 
+              mapping=aes(period, value),
+              size=1.3)
+  swfigure(sw,print,p,sw_option="height=3.5,width=7")
+  
+  p <- mipBarYearData(var[mainReg,y_bar,])
+  p <- p + theme(legend.position="none")
+  swfigure(sw,print,p,sw_option="height=4.5,width=7")
+  
+  p <- mipBarYearData(var[,y_bar,][mainReg,,,invert=TRUE]) +
+    guides(fill=guide_legend(ncol=3))
+  swfigure(sw,print,p,sw_option="height=9,width=8")
+  
+  swlatex(sw,"\\onecolumn")
+  p <- mipArea(var[mainReg,,,invert=TRUE],total=data[,,tot][mainReg,,,invert=TRUE],scales="free_y")
+  swfigure(sw,print,p,sw_option="height=8,width=16")
+  
+  
+  swlatex(sw,"\\twocolumn")
+  # line plot: land-use change emissions with national LULUCF accounting
+  p <- mipLineHistorical(data[mainReg,,"Emi|CO2|Land-Use Change|LULUCF national accounting (Mt CO2/yr)"],x_hist=histData[mainReg,,"Emi|CO2|Land Use (Mt CO2/yr)"],
+                         ylab='Emi|CO2|Land-Use Change|LULUCF national accounting [Mt CO2/yr]',scales="free_y",plot.priority=c("x_hist","x","x_proj"))
+  swfigure(sw,print,p,sw_option="height=8,width=8")
+  p <- mipLineHistorical(data[,,"Emi|CO2|Land-Use Change|LULUCF national accounting (Mt CO2/yr)"][mainReg,,,invert=TRUE],x_hist=histData[,,"Emi|CO2|Land Use (Mt CO2/yr)"][mainReg,,,invert=TRUE],
+                         ylab='Emi|CO2|Land-Use Change|LULUCF national accounting [Mt CO2/yr]',scales="free_y",plot.priority=c("x_hist","x","x_proj"),facet.ncol=3)
+  swfigure(sw,print,p,sw_option="height=9,width=8")
+  
+  }
 
   ## ---- Emissions GHG Sectors ----
   items <- c("Emi|GHG|Energy (Mt CO2eq/yr)",
@@ -1702,7 +1773,7 @@ compareScenarios <- function(mif, hist,
     
     swlatex(sw,"\\subsubsection{GHG Land-Use Change}")
     p <- mipLineHistorical(data[mainReg,,"Emi|GHG|Land-Use Change (Mt CO2eq/yr)"],
-                           ylab='Emi|GHG|Waste [Mt CO2eq/yr]',scales="free_y",plot.priority=c("x_hist","x","x_proj"))
+                           ylab='Emi|GHG|Land-Use Change [Mt CO2eq/yr]',scales="free_y",plot.priority=c("x_hist","x","x_proj"))
     swfigure(sw,print,p,sw_option="height=8,width=8")
     p <- mipLineHistorical(data[,,"Emi|GHG|Land-Use Change (Mt CO2eq/yr)"][mainReg,,,invert=TRUE],
                            ylab='Emi|GHG|Land-Use Change [Mt CO2eq/yr]',scales="free_y",plot.priority=c("x_hist","x","x_proj"),facet.ncol=3)
@@ -4241,7 +4312,7 @@ compareScenarios <- function(mif, hist,
     swfigure(sw,mipLineHistorical,data[,,"Temperature|Global Mean (K)"],x_hist=NULL,
              ylab='Temperature|Global Mean [K]',scales="free_y",plot.priority=c("x_hist","x","x_proj"),sw_option="height=10,width=9")
   }
-
+  
   ## Close output-pdf
   swclose(sw)
 }
