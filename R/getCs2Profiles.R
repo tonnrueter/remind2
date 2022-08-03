54f8b9ae6f47e3377d9166780618a8f39fc60b35
Christof Schoetz <cschoetz@pik-potsdam.de>
2022-08-03 10:33:34 +0200

    simplify arguments for getCs2Profiles

--- R/getCs2Profiles.R
+++ R/getCs2Profiles.R
@@ -1,24 +1,17 @@
 #' Get the List of cs2 Profiles
 #'
-#' A list of arguments to \code{\link{compareScenarios2}} is called a (cs2-) profile. This function loads a list of such profiles from a file. The default arguments are set such that the default profiles file of REMIND is loaded.
+#' A list of arguments to \code{\link{compareScenarios2}} is called a (cs2-) profile. This function loads a list of such profiles from a file. The default argument is set such that the default profiles file of REMIND is loaded.
 #'
-#' @param path \code{character(1)}. Relative or absolute path to the profiles file.
-#' @param relativePath \code{character(1)}. Path to the profiles file relative to the REMIND directory, i.e., \code{remindDir}.
-#' @param remindDir \code{character(1)}. Path to the REMIND directory.
+#' @param path \code{character(1)}. Path to the profiles file.
 #' @return A named list of profiles. The names are the names of the respective profile. Each profile is a named list. Those names correspond to arguments of \code{\link{compareScenarios2}}.
 #' @author Christof Schoetz
 #' @examples
 #' \dontrun{
 #' profiles <- getCs2Profiles() # load from default location
-#' profiles <- getCs2Profiles(remindDir = "path/to/remind")
 #' profiles <- getCs2Profiles(path = "path/to/profiles.json")
-#' profiles <- getCs2Profiles(relativePath = "path/relative/to/remindDir/profiles.json")
 #' }
 #' @export
-getCs2Profiles <- function(
-    path = file.path(remindDir, relativePath),
-    relativePath = "scripts/cs2/profiles.json",
-    remindDir = ".") {
+getCs2Profiles <- function(path = "./scripts/cs2/profiles.json") {
   path <- normalizePath(path, mustWork = TRUE)
   profiles <- jsonlite::read_json(path, simplifyVector = FALSE)
   # Remove entries starting with "_". They are treated as comments.
