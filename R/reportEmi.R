04af2b0c5e977261d8632a719195f765ef212705
Felix Schreyer <schreyer@pik-potsdam.de>
2023-06-29 15:04:00 +0200

    fix bug with Emi|GHG/CO2|w/o Non-energy Use|LULUCF national accounting variables, exclude bunkers from those variables as for standard totals variables

--- R/reportEmi.R
+++ R/reportEmi.R
@@ -2232,6 +2232,18 @@ reportEmi <- function(gdx, output = NULL, regionSubsetList = NULL, t = c(seq(200
     emi.vars.wBunkers <- c(emi.vars.wBunkers,
                                    "Emi|GHG|LULUCF national accounting (Mt CO2eq/yr)",
                                    "Emi|CO2|LULUCF national accounting (Mt CO2/yr)" )
+
+    # add bunker correction for emissions variables with LULUCF national accounting and w/o non-energy use
+    if ("FE|Non-energy Use|Industry (EJ/yr)" %in% getNames(output) &&
+        "FE|Non-energy Use|Industry|+|Liquids (EJ/yr)" %in% getNames(output) &&
+        "FE|Non-energy Use|Industry|+|Gases (EJ/yr)" %in% getNames(output) &&
+        "FE|Non-energy Use|Industry|+|Solids (EJ/yr)" %in% getNames(output)) {
+
+      emi.vars.wBunkers <- c(emi.vars.wBunkers,
+                             "Emi|GHG|w/o Non-energy Use|LULUCF national accounting (Mt CO2eq/yr)",
+                             "Emi|CO2|w/o Non-energy Use|LULUCF national accounting (Mt CO2/yr)" )
+
+    }
   }
 
   # variable names for emission variables with bunkers, insert w/ Bunkers
