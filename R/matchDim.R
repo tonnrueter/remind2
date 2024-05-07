917e535f9eb0f54aac32d876a109be39c8c3ce55
Falk Benke <falk.benke@pik-potsdam.de>
2024-05-07 12:00:31 +0200

    ensure matching regions for fuel price in reportLCOE

--- R/matchDim.R
+++ R/matchDim.R
@@ -10,7 +10,6 @@
 #'
 #' @export
 matchDim <- function(x, ref, fill = 0) {
-
   if (ndim(x, dim = 3) != ndim(ref, dim = 3)) {
     stop(
       "Unsupported case: magclass objects x and ref have different number of ",
@@ -34,4 +33,58 @@ matchDim <- function(x, ref, fill = 0) {
   r <- r[getRegions(ref), getYears(ref), getNames(ref)]
 
   return(r)
+}
+
+#' match spatial dimension of a magclass object
+#'
+#' A helper that restricts and expands a magclass object x to the size of a
+#' magclass object ref in the spatial dimension.
+#'
+#' @param x a magclass object to be modified
+#' @param ref a magclass object used as a reference for the modification
+#' @param fill value to be set in new dimensions
+#' @importFrom magclass ndim
+#'
+#' @export
+matchRegions <- function(x, ref, fill = 0) {
+  r <- new.magpie(
+    cells_and_regions = union(getRegions(x), getRegions(ref)),
+    years = getYears(x),
+    names = getNames(x),
+    fill = fill,
+    sets = names(dimnames(x))
+  )
+
+  # copy over values from x
+  r[getRegions(x), , ] <- x
+
+  # restrict object to dimensions of ref
+  r <- r[getRegions(ref), , ]
+}
+
+#' match temporal dimension of a magclass object
+#'
+#' A helper that restricts and expands a magclass object x to the size of a
+#' magclass object ref in the temporal dimension.
+#'
+#' @param x a magclass object to be modified
+#' @param ref a magclass object used as a reference for the modification
+#' @param fill value to be set in new dimensions
+#' @importFrom magclass ndim
+#'
+#' @export
+matchYears <- function(x, ref, fill = 0) {
+  r <- new.magpie(
+    cells_and_regions = getRegions(x),
+    years = union(getYears(x), getYears(ref)),
+    names = getNames(x),
+    fill = fill,
+    sets = names(dimnames(x))
+  )
+
+  # copy over values from x
+  r[, getYears(x), ] <- x
+
+  # restrict object to dimensions of ref
+  r <- r[, getYears(ref), ]
 }
